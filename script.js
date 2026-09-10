6b3dd5407e87c57f93de70fa68bab030f25bc8d8
João Vitor Londe <joao.9280899@aluno.mg.gov.br>
2026-09-10 15:47:39 -0300

    organiza inicialização dos scripts JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+javascript
+// script.js
+
+import { initMenuToggle } from './src/js/nav.js';
+import { initTestimonials } from './src/js/testimonials.js';
+
+// Inicializa as funcionalidades quando a página carregar
+document.addEventListener('DOMContentLoaded', () => {
+    initMenuToggle();
+    initTestimonials();
+});
+
